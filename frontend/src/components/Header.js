0ecfc55cd8da8d2cc3fa4c5909150f3496a0b488
Manik <manikrawat@outlook.com>
2021-03-12 19:59:12 +0530

    header fixed

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -3,24 +3,52 @@ import { Container, Nav, Navbar } from 'react-bootstrap'
 
 const Header = () => {
   return (
-    <header>
-      <Navbar bg='light' expand='lg'>
+    <>
+      <nav class='navbar navbar-expand-lg navbar-dark bg-primary'>
         <Container>
-          <Navbar.Brand href='/'>eKart</Navbar.Brand>
-          <Navbar.Toggle aria-controls='basic-navbar-nav' />
-          <Navbar.Collapse id='basic-navbar-nav'>
-            <Nav className='ml-auto'>
-              <Nav.Link href='/cart'>
-                <i className='fas fa-shopping-cart'></i> Cart
-              </Nav.Link>
-              <Nav.Link href='/login'>
-                <i className='fas fa-user'></i> Sign In
-              </Nav.Link>
-            </Nav>
-          </Navbar.Collapse>
+          <a class='navbar-brand' href='/'>
+            Rev-Taku
+          </a>
+          <button
+            class='navbar-toggler'
+            type='button'
+            data-toggle='collapse'
+            data-target='#navbarColor01'
+            aria-controls='navbarColor01'
+            aria-expanded='false'
+            aria-label='Toggle navigation'
+          >
+            <span class='navbar-toggler-icon'></span>
+          </button>
+
+          <div class='collapse navbar-collapse' id='navbarColor01'>
+            <ul class='navbar-nav mr-auto'>
+              <li class='nav-item active'>
+                <a class='nav-link' href='/movies'>
+                  Movies
+                  <span class='sr-only'>(current)</span>
+                </a>
+              </li>
+              <li class='nav-item'>
+                <a class='nav-link' href='/series'>
+                  Series
+                </a>
+              </li>
+              <li class='nav-item'>
+                <a class='nav-link' href='/animes'>
+                  Anime
+                </a>
+              </li>
+              <li class='nav-item'>
+                <a class='nav-link' href='/about'>
+                  About
+                </a>
+              </li>
+            </ul>
+          </div>
         </Container>
-      </Navbar>
-    </header>
+      </nav>
+    </>
   )
 }
 
