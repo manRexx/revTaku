59a5186683e240988c7ed2f7c96e434a5ad17489
Manik <manikrawat@outlook.com>
2021-03-12 19:36:29 +0530

    Footer added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,18 @@
 import './App.css'
+import { Container } from 'react-bootstrap'
+import Footer from './components/Footer'
 
 function App() {
-  return <>MANIK</>
+  return (
+    <>
+      <Container>
+        <main className='py-3'>
+          <h1>Hello</h1>
+        </main>
+      </Container>
+      <Footer />
+    </>
+  )
 }
 
 export default App
---
+++ frontend/src/components/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Col, Container, Row } from 'react-bootstrap'
+
+const Footer = () => {
+  return (
+    <Container>
+      <Row>
+        <Col className='text-center py-3'>Copyright &copy; eKart</Col>
+      </Row>
+    </Container>
+  )
+}
+
+export default Footer
