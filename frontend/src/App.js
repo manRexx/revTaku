cdbc179aefeae42dc112d446a884d8befd51c7a1
Manik <manikrawat@outlook.com>
2021-03-12 19:38:29 +0530

    Header

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,12 @@
 import './App.css'
 import { Container } from 'react-bootstrap'
 import Footer from './components/Footer'
+import Header from './components/Header'
 
 function App() {
   return (
     <>
+      <Header />
       <Container>
         <main className='py-3'>
           <h1>Hello</h1>
